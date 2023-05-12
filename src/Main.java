bfa58e643d27fae492a7ee6aa12d556979499a34
Mehemmed <ilyazovmehemmed@gmail.com>
2023-05-12 19:13:17 +0400

    added the lists that will be chooses

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,36 @@ public class Main {
         arrAnswer.add("100");
         arrAnswer.add("Paris");
         arrAnswer.add("Helium");
+
+        List<String> array1=new ArrayList<>();
+        array1.add("45");
+        array1.add("100");
+        array1.add("60");
+        array1.add("78");
+        array1.add("0");
+
+        List<String> array2=new ArrayList<>();
+        array2.add("Ankara");
+        array2.add("Lima");
+        array2.add("London");
+        array2.add("Baku");
+
+        List<String> array3=new ArrayList<>();
+        array3.add("100");
+        array3.add("0");
+        array3.add("50");
+        array3.add("90");
+
+        List<String> array4=new ArrayList<>();
+        array4.add("Sydney");
+        array4.add("Los Angeles");
+        array4.add("New York City");
+        array4.add("Paris");
+
+        List<String> array5=new ArrayList<>();
+        array5.add("Helium");
+        array5.add("Oxygen");
+        array5.add("Carbon");
+        array5.add("Nitrogen");
     }
 }

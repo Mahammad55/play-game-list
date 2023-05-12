ff81e360c00282b79f2f84fcff3ce323585c9c70
Mehemmed <ilyazovmehemmed@gmail.com>
2023-05-12 18:56:03 +0400

    the questions list added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        List<String> arrQuestion=new ArrayList<>();
+        arrQuestion.add("How many seconds are in a minute?");
+        arrQuestion.add("Where s the capital of Turkey");
+        arrQuestion.add("At what temperature does the water boil?");
+        arrQuestion.add("What city contains the Eiffel Towel?");
+        arrQuestion.add("Which goes make the sound ridiculous?");
 
     }
 }
